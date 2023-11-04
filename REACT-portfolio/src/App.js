3e249773004b8252e1515891f07289e61233f582
evan-pieper <evanjpieper@gmail.com>
2023-11-04 01:12:52 -0700

    fixed errors from moving file structure around

--- REACT-portfolio/src/App.js
+++ REACT-portfolio/src/App.js
@@ -1,10 +1,11 @@
 // Structural imports
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 // Styling imports
 import './styles/App.css';
 // Component imports
-import { Header, Subheader, AboutMe, Work, Contact } from './components';
+import { Header, Subheader, AboutMe, Work, Contact } from './components/Components';
 
 function App() {
   return (
@@ -12,9 +13,17 @@ function App() {
       <Header />
       <Subheader />
       <main className="col" id="mainCol">
-        <AboutMe />
-        <Work />
-        <Contact />
+        <Router>
+          <NavLink to="/aboutMeSection">
+            <AboutMe />
+          </NavLink>
+          <NavLink to="/workSection">
+            <Work />
+          </NavLink>
+          <NavLink to="/contactSection">
+            <Contact />
+          </NavLink>
+        </Router>
       </main>
     </div>
   );
---
+++ REACT-portfolio/src/components/Components.js
@@ -0,0 +1,7 @@
+import AboutMe from "./AboutMe";
+import Contact from "./Contact";
+import Header from "./Header";
+import Subheader from "./Subheader";
+import Work from "./Work";
+
+export { AboutMe, Contact, Header, Subheader, Work };
