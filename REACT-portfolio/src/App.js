7b11d9223ca176b063e8aba00b5bc507c8fb6bdc
evan-pieper <evanjpieper@gmail.com>
2023-11-04 00:24:36 -0700

    finished work cards with embeded links to external project pages

--- REACT-portfolio/src/App.js
+++ REACT-portfolio/src/App.js
@@ -40,7 +40,25 @@ function AboutMe() {
 function Work() {
   return (
     <section className="work row" id="workSection">
-      {/* ... */}
+      <h1 class = "leftCol col">Work</h1>
+            
+      <section class = "rightCol row">
+        <a href = "https://aaronh-o.github.io/tier-ranker/">
+          <section class = "card" id = "firstCard">
+            <h1 class = "cardTag">First Project</h1>
+          </section>
+        </a>
+        <a href = "https://powerful-stream-45360-1940a41a4b54.herokuapp.com/login">
+          <section class = "card">
+            <h1 class = "cardTag">Second Project</h1>
+          </section>
+        </a>
+        <a href = "https://evan-pieper.github.io/Code-Quiz/">
+          <section class = "card">
+            <h5 class = "cardTag">Third Project</h5>
+          </section>
+        </a>
+      </section>
     </section>
   );
 }
