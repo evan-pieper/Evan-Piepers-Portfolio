2e9bb583e84353ae47ef95949f9755d21975f62a
evan-pieper <evanjpieper@gmail.com>
2023-11-04 00:53:07 -0700

    restructured file organization and deleted unnecessary files

--- REACT-portfolio/src/App.js
+++ REACT-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'link-react' ;
 
 function Header() {
   return (
---
+++ REACT-portfolio/src/components/Header.js
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+      <header class = "row" id = "header">
+          <section id = "header-name">Evan Pieper</section>
+          <section id = "navbar">
+              {/* Navbar links to rest of document, except for resume which redirects to a separate page (linkedin as a placeholder until separate resume page is integrated)*/}
+              <a href="#aboutMeSection">About Me</a>
+              <a href="#workSection">Work</a>
+              <a href="#contactSection">Contact</a>
+              <a href="https://www.linkedin.com/in/evanpieper/">Resume</a>
+          </section>
+      </header>
+  
+    );
+}
+
+export default Header;
