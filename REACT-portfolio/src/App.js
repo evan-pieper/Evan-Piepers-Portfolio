d3f98808465a1afa56a82d3946e076a3099225c3
evan-pieper <evanjpieper@gmail.com>
2023-11-04 01:02:28 -0700

    ported aboutme

--- REACT-portfolio/src/App.js
+++ REACT-portfolio/src/App.js
@@ -1,17 +1,10 @@
+// Structural imports
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+// Styling imports
 import './styles/App.css';
-import { Header, Subheader, Work, Contact } from './components';
-
-function AboutMe() {
-  return (
-    <section className="aboutMe row" id="aboutMeSection">
-      {/* placeholder for about me*/}
-      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-
-      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-    </section>
-  );
-}
+// Component imports
+import { Header, Subheader, AboutMe, Work, Contact } from './components';
 
 function App() {
   return (
