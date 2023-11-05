6986773e97c2507bd94f73e64d84120f3ef40ade
evan-pieper <evanjpieper@gmail.com>
2023-11-04 22:10:48 -0700

    commented components.js

--- REACT-portfolio/src/components/Components.js
+++ REACT-portfolio/src/components/Components.js
@@ -3,5 +3,5 @@ import Contact from "../pages/Contact";
 import Header from "../pages/Header";
 import Subheader from "./Subheader";
 import Work from "../pages/Work";
-
+// import all the components and export them as one object (not currently used but could be integrated if we want get away from export default)
 export { AboutMe, Contact, Header, Subheader, Work };
