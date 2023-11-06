bc71790657f4fba435ec0264399c8bb5f2809899
evan-pieper <evanjpieper@gmail.com>
2023-11-05 20:54:16 -0800

    modified gitignore

--- REACT-portfolio/src/pages/Contact.js
+++ REACT-portfolio/src/pages/Contact.js
@@ -10,6 +10,18 @@ function Contact() {
             <label htmlFor="emailInput">Email address</label>
             <input type="email" className="form-control" id="emailInput" placeholder="example@email.com"></input>
           </div>
+          <div className="form-group">
+            <label htmlFor="phoneInput">Phone Number</label>
+            <input type="tel" className="form-control" id="phoneInput" placeholder="123-456-7890"></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="subjectInput">Subject</label>
+            <input type="text" className="form-control" id="subjectInput" placeholder="Subject"></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="messageInput">Message</label>
+            <textarea className="form-control" id="messageInput" rows="3"></textarea>
+          </div>
         </form>
       </section>
     );
