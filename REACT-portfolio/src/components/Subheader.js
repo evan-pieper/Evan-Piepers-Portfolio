95e0e84c9f528ecf28f2146d361ae11e84c0c420
evan-pieper <evanjpieper@gmail.com>
2023-11-06 03:32:34 -0800

    deleted subheader as its not used

--- REACT-portfolio/src/components/Subheader.js
+++
@@ -1,9 +0,0 @@
-function Subheader() {
-    return (
-      <section className="row" id="subheader">
-        <h4 id = "subheader_subtitle">Cool Subtitle Here!</h4>
-      </section>
-    );
-}
-
-export default Subheader;
