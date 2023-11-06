486ad7e39af9d0d3f805d8e2eec6abfdc26feaa0
evan-pieper <evanjpieper@gmail.com>
2023-11-06 01:55:14 -0800

    formatted portfolio and portfolio cards. Also styles cards

--- REACT-portfolio/src/pages/Portfolio.js
+++ REACT-portfolio/src/pages/Portfolio.js
@@ -5,24 +5,24 @@ function Portfolio() {
       <div className="left col">
         <a href = "https://aaronh-o.github.io/tier-ranker/">
           <section className = "card" id = "firstCard">
-            <h1 className = "cardTag">First Project</h1>
+            <h1 className = "cardTag">Tier Ranker</h1>
           </section>
         </a>
         <a href = "https://powerful-stream-45360-1940a41a4b54.herokuapp.com/login">
           <section className = "card">
-            <h1 className = "cardTag">Second Project</h1>
+            <h1 className = "cardTag">Cro-X</h1>
           </section>
         </a>
       </div>
       <div className="right col">
         <a href = "https://evan-pieper.github.io/Code-Quiz/">
           <section className = "card">
-            <h5 className = "cardTag">Third Project (Need to finish)</h5>
+            <h1 className = "cardTag">Spell/Craft</h1>
           </section>
         </a>
         <a href = "https://github.com/evan-pieper">
           <section className = "card">
-            <h5 className = "cardTag">Placeholder</h5>
+            <h1 className = "cardTag">Placeholder</h1>
           </section>
         </a>
       </div>
