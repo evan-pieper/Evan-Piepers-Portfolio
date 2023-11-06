dd3a83c7b3f1ad787295b188fd28984ff2794e6e
evan-pieper <evanjpieper@gmail.com>
2023-11-05 17:01:48 -0800

    replaced navlink links with actual navlinks, so the color of the link turns white when it is the active link

--- REACT-portfolio/src/pages/Navbar.js
+++ REACT-portfolio/src/pages/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
     return (
         <section className='row' id = "navbar">
             {/* Navbar links to rest of document, except for resume which redirects to a separate page (linkedin as a placeholder until separate resume page is integrated)*/}
             <h1 className = "title">Evan Pieper</h1>
-            <Link to="/about-me">About me</Link>
-            <Link to="/portfolio">Portfolio</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to='/resume'>Resume</Link>
+            <NavLink to="/about-me">About me</NavLink>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to='/resume'>Resume</NavLink>
         </section>
     );
 }
