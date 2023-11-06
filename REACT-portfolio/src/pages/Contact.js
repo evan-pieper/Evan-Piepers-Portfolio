8ba362e64cfa8b169babcd7624037d3c52fdccc8
evan-pieper <evanjpieper@gmail.com>
2023-11-06 03:01:19 -0800

    finished contact

--- REACT-portfolio/src/pages/Contact.js
+++ REACT-portfolio/src/pages/Contact.js
@@ -1,21 +1,93 @@
+import React, { useState } from 'react';
+
 function Contact() {
+  const [formFields, setFormFields] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+  const [formValid, setFormValid] = useState(false);
+  const [emailError, setEmailError] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields(prevState => {
+      const newFields = { ...prevState, [name]: value };
+      const allFieldsFilled = Object.values(newFields).every(field => field.trim() !== '');
+      setFormValid(allFieldsFilled && !emailError);
+      return newFields;
+    });
+
+    if (name === 'email') {
+      validateEmail(value);
+    }
+  };
+
+  const validateEmail = (email) => {
+    // Regular expression to test a valid email format
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!re.test(email)) {
+      setEmailError('Invalid email address');
+      setFormValid(false);
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!emailError && formValid) {
+      // Perform the submission logic here
+      alert('This feature is not functional yet. Please check back later.');
+    }
+  };
+
   return (
     <div className="contact-form">
       <h2>Contact Me</h2>
-      <form action="your-server-side-handler" method="POST">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" required></input>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formFields.name}
+          onChange={handleInputChange}
+          required
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required></input>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formFields.email}
+          onChange={handleInputChange}
+          required
+        />
 
         <label htmlFor="phone">Callback Number</label>
-        <input type="tel" id="phone" name="phone" required></input>
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          value={formFields.phone}
+          onChange={handleInputChange}
+          required
+        />
 
         <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" required></textarea>
+        <textarea
+          id="message"
+          name="message"
+          value={formFields.message}
+          onChange={handleInputChange}
+          required
+        />
 
-        <button type="submit">Send</button>
+        <button type="submit" disabled={!formValid}>Send</button>
+        {emailError && <p className="error-message">{emailError}</p>}
       </form>
     </div>
   );
