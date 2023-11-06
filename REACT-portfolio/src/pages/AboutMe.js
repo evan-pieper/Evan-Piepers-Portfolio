974d86b1de68d3403624d91e7eec8fe4c8f101c1
evan-pieper <evanjpieper@gmail.com>
2023-11-05 22:35:36 -0800

    added headshot to aboutme section, uploaded corresponding photo, and styled image element

--- REACT-portfolio/src/pages/AboutMe.js
+++ REACT-portfolio/src/pages/AboutMe.js
@@ -1,8 +1,10 @@
+import aboutMePic from "../styles/images/aboutMePic.jpg";
 function AboutMe() {
     return (
       <section className="col" id="aboutMeSection">
         {/* placeholder for about me*/}
         <h2>About Me</h2>
+        <img src={aboutMePic} alt="Evan Pieper's Headshot" className="picture" id = "aboutMePic"/>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
   
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
