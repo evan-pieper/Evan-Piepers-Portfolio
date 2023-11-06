ca4f98bb6782343229b6a583823fef23d3c5ff73
evan-pieper <evanjpieper@gmail.com>
2023-11-06 03:34:49 -0800

    updated navbar

--- REACT-portfolio/src/pages/Navbar.js
+++ REACT-portfolio/src/pages/Navbar.js
@@ -6,10 +6,10 @@ function Navbar() {
         <section className='row' id = "navbar">
             {/* Navbar links to rest of document, except for resume which redirects to a separate page (linkedin as a placeholder until separate resume page is integrated)*/}
             <h1 className = "title">Evan Pieper</h1>
-            <NavLink to="/about-me">About me</NavLink>
-            <NavLink to="/portfolio">Portfolio</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-            <NavLink to='/resume'>Resume</NavLink>
+            <NavLink to="/about-me" activeClassName="active">About me</NavLink>
+            <NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink>
+            <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+            <NavLink to='/resume' activeClassName="active">Resume</NavLink>
         </section>
     );
 }
